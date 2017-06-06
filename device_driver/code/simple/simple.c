872dc5f6edb77ef4abb800bb93af7da4bcd58e37
Mikkel <mikkel.jae@gmail.com>
2017-06-06 14:59:26 +0200

    changed simple.c (hopefully not Makefile)

--- device_driver/code/simple/simple.c
+++ device_driver/code/simple/simple.c
@@ -3,13 +3,13 @@
 #include <linux/kernel.h>           // Contains types, macros, functions for the kernel
  
 MODULE_LICENSE("GPL");              ///< The license type -- this affects runtime behavior
-MODULE_AUTHOR("Derek Molloy");      ///< The author -- visible when you use modinfo
-MODULE_DESCRIPTION("A simple Linux driver for the BBB.");  ///< The description -- see modinfo
+MODULE_AUTHOR("Mikkel and Thomas");      ///< The author -- visible when you use modinfo
+MODULE_DESCRIPTION("A simple Linux driver.");  ///< The description -- see modinfo
 MODULE_VERSION("0.1");              ///< The version of the module
  
-static char *name = "world";        ///< An example LKM argument -- default value is "world"
-module_param(name, charp, S_IRUGO); ///< Param desc. charp = char ptr, S_IRUGO can be read/not changed
-MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");  ///< parameter description
+//static char *name = "world";        ///< An example LKM argument -- default value is "world"
+//module_param(name, charp, S_IRUGO); ///< Param desc. charp = char ptr, S_IRUGO can be read/not changed
+//MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");  ///< parameter description
  
 /** @brief The LKM initialization function
  *  The static keyword restricts the visibility of the function to within this C file. The __init
@@ -17,8 +17,8 @@ MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");  ///< parame
  *  time and that it can be discarded and its memory freed up after that point.
  *  @return returns 0 if successful
  */
-static int __init helloBBB_init(void){
-   printk(KERN_INFO "EBB: Hello %s from the BBB LKM!\n", name);
+static int __init hello_init(void){
+   printk(KERN_INFO "Hello World!, from your device.\n");
    return 0;
 }
  
@@ -26,13 +26,13 @@ static int __init helloBBB_init(void){
  *  Similar to the initialization function, it is static. The __exit macro notifies that if this
  *  code is used for a built-in driver (not a LKM) that this function is not required.
  */
-static void __exit helloBBB_exit(void){
-   printk(KERN_INFO "EBB: Goodbye %s from the BBB LKM!\n", name);
+static void __exit hello_exit(void){
+   printk(KERN_INFO "Goodbye World!, from your device.\n");
 }
  
 /** @brief A module must use the module_init() module_exit() macros from linux/init.h, which
  *  identify the initialization function at insertion time and the cleanup function (as
  *  listed above)
  */
-module_init(helloBBB_init);
-module_exit(helloBBB_exit);
+module_init(hello_init);
+module_exit(hello_exit);
